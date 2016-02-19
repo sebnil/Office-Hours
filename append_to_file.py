441599d426506e43f2cb18ef8f14d133051ee606
unknown <seni@SPECTRE039.cpac.loc>
2016-02-19 14:13:35 +0100

    better solution of finding absolute paths automatically.

---
+++ append_to_file.py
@@ -0,0 +1,17 @@
+import sys
+from time import gmtime, strftime
+#strftime("%Y-%m-%d %H:%M:%S", gmtime())
+import os
+
+# change current directory to script directory
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+try:
+    action = sys.argv[1]
+except:
+    action = '?'
+
+with open(r"C:\ProjectArea\Office-Hours\office_hours.txt", "a") as myfile:
+    myfile.write("{action}\t{time}\n".format(action=action, time=strftime("%Y-%m-%d %H:%M:%S", gmtime())))
---
+++ logon.bat
@@ -0,0 +1,1 @@
+python %~dp0\append_to_file.py IN
