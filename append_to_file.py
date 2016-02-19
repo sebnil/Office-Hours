242333e58f3ee689c10c5405cdbbdf24bfdf520d
Sebastian Nilsson <sebnil@gmail.com>
2016-02-19 14:30:32 +0100

    commenting code

--- append_to_file.py
+++ append_to_file.py
@@ -7,10 +7,12 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
+# get first argument. usually "IN" or "OUT" but could be any string
 try:
     action = sys.argv[1]
 except:
     action = '?'
 
+# append to log file
 with open(r"C:\ProjectArea\Office-Hours\office_hours.txt", "a") as myfile:
     myfile.write("{action}\t{time}\n".format(action=action, time=strftime("%Y-%m-%d %H:%M:%S", gmtime())))
